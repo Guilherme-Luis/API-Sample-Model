38c68611b4333c88a4caa643df5ad1c06d95b2c8
GuilhermeLuis242 <guilherme.rodrigues@ecenture.com.br>
2026-05-06 23:21:55 -0300

    fix: force cors headers for vercel requests

--- src/app.js
+++ src/app.js
@@ -8,10 +8,18 @@ const app = express();
 const { NODE_ENV } = process.env;
 
 //É errado, mas fazer o que né?
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://frontend-sample-model.vercel.app');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
