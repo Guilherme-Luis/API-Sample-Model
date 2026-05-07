7168fc5778b9b4fedc0187e96971f5de9ff41508
GuilhermeLuis242 <guilherme.rodrigues@ecenture.com.br>
2026-05-07 00:14:06 -0300

    fix: add express preflight options handler

--- src/app.js
+++ src/app.js
@@ -12,6 +12,15 @@ app.use(cors({
     credentials: true
 }));
 
+app.options(/(.*)/, (req, res) => {
+    res.header('Access-Control-Allow-Origin', 'https://frontend-sample-model.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    return res.sendStatus(200);
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
